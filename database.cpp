3ada83b5db05625b95cdc28c9ae4fadedaf31758
Владимир Ловцов <lovets1818@gmail.com>
2021-12-07 22:18:00 +0300

    mysql and user + testing

--- database.cpp
+++ database.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <map>
 #include <vector>
-#include <mysql/mysql.h>
+//#include <mysql/mysql.h>
+#include <mysql_connection.h> // include path /mysql-cppcon-8
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
 using namespace std;
 
 
@@ -14,97 +20,154 @@ public:
 	ConnectorDb(const ConnectorDb&) = delete;
 	ConnectorDb & operator= (const ConnectorDb&) = default;
 	virtual ~ConnectorDb() = default;
-	virtual void Add(mapstr& data) = 0;
-	virtual void Delete(const string& table, int id) = 0;
-	virtual void Edit(mapstr& newData) = 0;
-	virtual mapstr Get(vecstr& cols) = 0;
+	virtual void Add(string& table, mapstr& data) = 0;
+	virtual void Delete(const string& table, string condition) = 0;
+	virtual void Edit(string& table, mapstr& newData, string condition) = 0;
+	virtual vector<mapstr> Get(string& table, vecstr& cols, string condition = "") = 0;
 
 };
 
 class ConnectorMySql:public ConnectorDb {
 public:
 	ConnectorMySql(const string& host, const string& usr, const string& psw, const string& db, const int port) { 
-		mysqlconn = mysql_init(NULL);
-
-		// Connect to database 
-		if (!mysql_real_connect(mysqlconn, host.c_str(), usr.c_str(), psw.c_str(), db.c_str(), port, NULL, 0)) {
-			fprintf(stderr, "%s\n", mysql_error(mysqlconn));
-			exit(1);
-		}	
+		// Create a connection 
+		driver = get_driver_instance();
+		con = driver->connect("tcp://127.0.0.1:3306", usr, psw);
+		// Connect to the MySQL test database 
+		con->setSchema("Kinopoiskdata");
+		stmt = con->createStatement();
+		
+		
+		cout << "Created mysql\n";
 	};
+
+	//ConnectorMySql() = default;
+	
 	~ConnectorMySql() {
-		mysql_free_result(res);
-      	mysql_close(mysqlconn);
+		
+		cout << "NOT Closed\n";
+		if (res != nullptr) delete res;
+		delete stmt;
+		delete con;
+		cout << "Closed mysql\n";
 
 	};
-	void Add(map<string, string>& data) override {
-		string sqlquery = "insert into " + data["table"] + " (";
+	void Add(string& table, map<string, string>& data) override {
+		cout <<"INSIDE add\n";
+		string sqlquery = "insert into " + table + " (";
 		mapstr :: iterator it;
 		
-		for (it = data.begin(); it != data.end(); it++) { 
-
-			sqlquery += it->first + ", ";
+		for (auto& it : data) { 
+			sqlquery += it.first + ", ";
 		}
+		sqlquery.pop_back();
+		sqlquery.pop_back();
 		sqlquery += ") values (";
 
-		for (it = data.begin(); it != data.end(); it++) { 
-			sqlquery += it->first + ", ";
+		for (auto& it : data) { 
+			sqlquery += "\"" + it.second + "\", ";
 		}
 		sqlquery.pop_back();
+		sqlquery.pop_back();
+		
 		sqlquery += ");";
 
-		if (mysql_query(mysqlconn, sqlquery.c_str())) {
-          fprintf(stderr, "%s\n", mysql_error(mysqlconn));
-          exit(1);
-      }
+		cout <<"QUERY:: " << sqlquery << endl;
+
+		stmt->execute(sqlquery);
+
 	};
-	void Delete(const string& table, int id) override {
-		string sqlquery = "delete " + table + " where id = " + std::to_string(id) + ";";
-		map<string, string> :: iterator it;
+
+	void Delete(const string& table, string condition) override {
+		string sqlquery = "delete from " + table + " where " + condition + ";";
+		cout <<"QUERY:: " << sqlquery << endl;
+		stmt->execute(sqlquery);
 		
-		if (mysql_query(mysqlconn, sqlquery.c_str())) {
-          fprintf(stderr, "%s\n", mysql_error(mysqlconn));
-          exit(1);
-      	}
 	};
 
-	void Edit(mapstr& newData) override {
-		string sqlquery = "update " + newData["tablename"] + " set ";
-		mapstr :: iterator it;
-		for (it = newData.begin(); it != newData.end(); it++) { 
-			sqlquery += it->first +" = " + it->second + ", ";
+	void Edit(string& table, mapstr& newData, string condition) override {
+		string sqlquery = "update " + table + " set ";
+		//mapstr :: iterator it;
+		for (auto& it : newData) { 
+			sqlquery += it.first +" = \"" + it.second + "\", ";
 		}
 		sqlquery.pop_back();
-		sqlquery += ";";
+		sqlquery.pop_back();
 
-		if (mysql_query(mysqlconn, sqlquery.c_str())) {
-          fprintf(stderr, "%s\n", mysql_error(mysqlconn));
-          exit(1);
-      	}
+		sqlquery += " where ";
+
+		sqlquery += condition + ";";
+
+		cout <<"QUERY:: " << sqlquery << endl;
+
+		stmt->execute(sqlquery);
 
 	};
-	mapstr Get(vecstr& cols) override {
-		mapstr answer;
-		string sqlquery = "select ";// + " from " + cols[1] + ";"";
-		vecstr::iterator it;
-		for (it = cols.begin(); it != cols.end(); it++) { 
-			sqlquery += *it + ", ";
+	
+	vector<mapstr> Get(string& table, vecstr& cols, string condition = "") override {
+		vector<mapstr> answer(0);
+
+		string sqlquery = "select ";
+		
+		for (auto& it : cols) {
+			sqlquery += it + ", ";
 		}
 		sqlquery.pop_back();
-		sqlquery += " from " + cols[1] + ";";
+		sqlquery.pop_back();
 
-		if (mysql_query(mysqlconn, sqlquery.c_str())) {
-          fprintf(stderr, "%s\n", mysql_error(mysqlconn));
-          exit(1);
-      	}
-		res = mysql_store_result(mysqlconn);
-		return answer;
+		sqlquery += " from " + table;// +";";
+		if (condition != "") sqlquery += " where " + condition;
+		sqlquery += + " order by " + *cols.begin() + ";";
+
+		cout <<"QUERY:: " << sqlquery << endl;
+		bool exist = false;
+		//try {
+			res = stmt->executeQuery(sqlquery);
+			exist = res->next();
+			cout <<"EXIST: " << exist <<endl;
+			if (!exist) {
+				cout <<"NOT EXIST\n";
+				
+			}
+			else {
+				cout <<"EXIST\n";
+				res->beforeFirst();
+			//cout << res->isNull(1) <<endl;
+				while (res->next()) {
+					cout <<"IN\n";
+					mapstr buffer = mapstr();
+					for (auto& it : cols) {
+						string temp = res->getString(it);
+
+						cout <<"RES: " << res->wasNull() <<endl;
+						buffer[it] = temp;
+						//cout << it << endl;
+						//answer[it].push_back(temp);
+						//cout << " " << temp;
+						
+					}
+					answer.push_back(buffer);
+					//cout << endl;
+						}
+					//}
+					//else {
+					//	cout<<"EMPTY";
+					//	return {};
+						
+					//}
+				
+				cout << endl;
+				
+			}
+			cout <<"END\n";
+		return answer;	
 	};
 
 private:
-	MYSQL *mysqlconn;
-    MYSQL_RES *res;
-    MYSQL_ROW row;
+	sql::Driver *driver;
+	sql::Connection *con;
+	sql::ResultSet *res = nullptr;
+	sql::Statement *stmt;
 };
 
 
@@ -122,41 +185,233 @@ protected:
 class UserConnector:public Connector {
 public:
 	UserConnector(const string& host, const string& usr, const string& psw, const string& db, const int port) { 
-		*conn = ConnectorMySql(host, usr, psw, db, port);
+		ConnectorMySql* buffer = new ConnectorMySql(host, usr, psw, db, port);
+		conn = buffer;
+		//*conn = ConnectorMySql(host, usr, psw, db, port);
+		cout << "USER CREATED\n";
 	}
-	~UserConnector() {};
-	void AddUser(mapstr data) {};
-	void DeleteUser(int id) {};
-	void EditUser(int id, mapstr data) {};
-	mapstr GetUserInfo(int id, vecstr cols) {
-		mapstr m;
-		return m;
+	~UserConnector() {
+		delete conn;
+		cout << "USER DESTRUCTOR\n";
+		
 	};
-	void EstimateFilm(int userid, int film, int estimate) {};
-	void CommentFilm(int userid, int filmid, string text) {};
-	vecstr GetCommentsFilm(int userid, int filmid) {
-		vecstr v;
-		return v;
-	};
-	void DeleteComment(int userid, int filmid) {};
-	mapstr GetUserEstimate(int id) {
-		mapstr m;
-		return m;
-	};
-	vecstr GetUsersId(string condition) {
-		vecstr v;
-		return v;
+	void AddUser(string& email, string& username, string& password, 
+				string gender = "", int age = -1, vecstr format = { },
+				vecstr genre = { }, vecstr director = { }, int iduser = -1) {
+		
+		mapstr data; 
+		if (iduser > 0) data["iduser"] = to_string(iduser);
+		data["email"] = email;
+		data["username"] = username;
+		data["password"] = password;
+		if (gender != "") data["gender"] = gender;
+		if (age > 0) data["age"] = to_string(age);
+		cout <<"BEFORE\n";
+		string table = "user";
+		conn->Add(table, data);
+
+		string condition = "username = \"" + data["username"] + "\"";
+		vecstr userid = {"iduser"};
+
+		vector<mapstr> res = conn->Get(table, userid, condition);
+		string id = res[0]["iduser"];
+
+		cout << id << endl;
+
+		if (genre.size()) {
+			AddUserPreference(id, "genre", genre);
+		}
+
+		if (director.size()) {
+			AddUserPreference(id, "director", director);
+			
+		}
+
+		if (format.size()) {
+			AddUserPreference(id, "format", format);
+			
+		}
+
 	};
 
-	map<string, vector<string>> GetDataFrame() { };
-private:
-	void AddUserPreference(mapstr data);
-	void EditUserPreference(mapstr data);
-	void DeleteUserPreference(mapstr data);
-	mapstr GetUserPreference(int id);
-	int FindUserEstimation(int userid, int filmid);
+	void DeleteUser(int id) {
+		string table = "user";
+		string condition = "iduser = " + to_string(id);
+		conn->Delete(table, condition);
+	};
+
+	string GetUserId(string& username) {
+		string table = "user";
+		string condition = "username = \"" + username + "\"";
+		vecstr userid = {"iduser"};
+
+		vector<mapstr> res = conn->Get(table, userid, condition);
+		string id = res[0]["iduser"];
+		return id;
+	}
+
+	void EditUser(int id, mapstr data) {
+		string table = "user";
+		string condition = "iduser = " + to_string(id);
+		conn->Edit(table, data, condition);
+
+	};
+
+	mapstr GetUserInfo(int id, vecstr cols) {
+		string table = "user";
+		string condition = " iduser = " + to_string(id);
+		mapstr userinfo = conn->Get(table, cols, condition)[0];
+		//mapstr m;
+		return userinfo;
+	};
+
+	void EstimateFilm(int userid, int film, int estimate) {
+		string table = "estimation";
+		mapstr data;
+		data["user_iduser"] = to_string(userid);
+		data["film_idfilm"] = to_string(film);
+		data["estimate"] = to_string(estimate);
+		conn->Add(table, data);
+	};
+
+	void CommentFilm(int userid, int filmid, string text) {
+		string table = "comment";
+		mapstr data;
+		data["user_iduser"] = to_string(userid);
+		data["film_idfilm"] = to_string(filmid);
+		data["commenttext"] = text;
+		conn->Add(table, data);
+	};
+
+	vecstr GetCommentsFilm(int userid, int filmid) {
+		string table = "comment";
+		string condition = " user_iduser = " + to_string(userid) + " and film_idfilm = " + to_string(filmid);
+		vecstr cols = {"commenttext"};
+		vector<mapstr> commentmap = conn->Get(table, cols, condition);
+		vecstr commentlist(0);
+
+		for (auto& it : commentmap) {
+			commentlist.push_back(it["commenttext"]);
+		}
+		return commentlist;
+	};
+
+	void DeleteComment(int commentid) {
+		string table = "comment";
+		string condition = "idcomment = " + to_string(commentid);
+		conn->Delete(table, condition);
+	};
+
+
+	vector<mapstr> GetUserEstimate(int id) {
+		string table = "estimation";
+		vecstr cols = {"film_idfilm", "estimate"};
+		string condition = "user_iduser = " + to_string(id);
+		vector<mapstr> est = conn->Get(table, cols, condition);
+		return est;
+	};
+
+	vecstr GetUsersId(string condition = "") {
+		string table = "user";
+		vecstr cols = {"iduser"};
+		vector<mapstr> ids = conn->Get(table, cols, condition);
+		vecstr idlist(0);
+		for (auto& it : ids) {
+			idlist.push_back(it["iduser"]);
+		}
+		return idlist;
+	};
+
+	map<string, vector<string>> GetDataFrame() {
+		string table = "user"; 
+		vecstr cols = {"iduser", "age", "gender"};
+		map<string, vector<string>> df;
+
+		vector<map<string, vecstr>> dataframe;
+
+		vector<mapstr> userbase = conn->Get(table, cols);
+
+		// add base fileds
+		for (auto& row : userbase) {
+			map<string, vecstr> temp;
+			temp["iduser"] = {row["iduser"]};
+			temp["age"] = {row["age"]};
+			temp["age"] = {row["age"]};
+
+
+			// add preferences ????????????????????//
+
+			// add estimations
+			temp["estimate"] = vecstr(0);
+
+			vector<mapstr> estimates = GetUserEstimate(stoi(row["iduser"]));
+			for (auto& pair : estimates) {
+				temp["estimate"].push_back(pair["film_idfilm"] + " " + pair["estimate"]);
+			}
+			dataframe.push_back(temp);
+		}
+
+		
+		return df;
+	};
+
+//private:
+	void AddUserPreference(string userid, string tab, vecstr data) {
+		string table = "preference_" + tab;
+		for (auto& it : data) {
+				mapstr temp;
+				temp[tab] = it;
+				temp["user_iduser"] = userid;
+				conn->Add(table, temp);
+			}
+	};
+
+	/*void EditUserPreference(int id, mapstr data) {
+
+	};*/
+	void DeleteUserPreference(int id, multimap<string, string> data) {
+		for (auto& pair : data) {
+			string table = "preference_" + pair.first;
+			string condition = " user_iduser = " + to_string(id) + " and " + pair.first + 
+								" = \"" + pair.second + "\""; 
+			conn->Delete(table, condition);
+		}
+	};
+
+	vector<mapstr> GetUserPreference(int id) {
+		string condition = "user_iduser = " + to_string(id);
+		vecstr cols;
+		string tab_genre = "preference_genre";
+		cols = {"genre"};
+		vector<mapstr> genre = conn->Get(tab_genre, cols, condition);
+
+		string tab_format = "preference_format";
+		cols = {"format"};
+		vector<mapstr> format = conn->Get(tab_format, cols, condition);
+
+		string tab_director = "preference_director";
+		cols = {"director"};
+		vector<mapstr> director = conn->Get(tab_director, cols, condition);
+
+		vector<mapstr> preference;
+		preference.insert(preference.end(), genre.begin(), genre.end());
+		preference.insert(preference.end(), format.begin(), format.end());
+		preference.insert(preference.end(), director.begin(), director.end());
+
+		return preference;
+	};
+
+	int FindUserEstimation(int userid, int filmid) {
+		string table = "estimation";
+		string condition = " user_iduser = " + to_string(userid) + " and film_idfilm = " + to_string(filmid);
+		vecstr cols = {"estimate"};
+		int estim = stoi(conn->Get(table, cols, condition)[0]["estimate"]);
+		return estim;
+	};
 
 };
+
+
 
 class FilmConnector :public Connector {
 public:
--- database_test.cpp
+++ database_test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "database.cpp"
+#include <chrono>
 #define private public
 
 
@@ -13,50 +14,75 @@ TEST(ConnectorMySql_create, Assert_True) {
 
 TEST(ConnectorMySql_Add_Get, Assert_True) {
   ConnectorMySql testcon("localhost", "vladimir", "12345678", "Kinopoisdata", 3306);
-  mapstr data = {{"table","user"},
+  string table = "user";
+  mapstr data = {
     {"email", "mail@mail.ru"},
-    {"name", "Lionel Messi"},
+    {"username", "Lionel Messi"},
     {"password", "1899"}
   };
-  testcon.Add(data);
+  testcon.Add(table, data);
 
-  vecstr columns = {"table", "email", "name", "password"};
+  vecstr columns = {"email", "username", "password"};
 
-  mapstr result = testcon.Get(columns);
+  string condition = "username = \"Lionel Messi\"";
+  mapstr result = testcon.Get(table, columns, condition)[0];
+
+
 
   EXPECT_EQ(data, result);
+
+  testcon.Delete(table, condition);
 
   //EXPECT_TRUE(true);
 }
 
 TEST(ConnectorMySql_Delete, Assert_True) {
-  ConnectorMySql testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
-  mapstr data = {{"table","user"},
+  ConnectorMySql testcon("localhost", "vladimir", "12345678", "Kinopoisdata", 3306);
+  string table = "user";
+  mapstr data = {
     {"email", "mail@mail.ru"},
-    {"name", "Lionel Messi"},
+    {"username", "Lionel Messi"},
     {"password", "1899"}
   };
-  testcon.Add(data);
-  testcon.Delete("user", 1);
-  vecstr columns = {"table", "email", "name", "password"};
-  mapstr result = testcon.Get(columns);
+  testcon.Add(table, data);
+
+  vecstr columns = {"email", "username", "password"};
+
+  string condition = "username = \"Lionel Messi\"";
+
+  testcon.Delete(table, condition);
+
+  mapstr result {};// = testcon.Get(table, columns, condition)[0];
+
   mapstr exp = {};
   EXPECT_EQ(exp, result);
 }
 
 TEST(ConnectorMySql_Edit, Assert_True) {
-  ConnectorMySql testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
-  mapstr data = {{"table","user"},
+  ConnectorMySql testcon("localhost", "vladimir", "12345678", "Kinopoisdata", 3306);
+  string table = "user";
+  mapstr data = {
     {"email", "mail@mail.ru"},
-    {"name", "Lionel Messi"},
+    {"username", "Lionel Messi"},
     {"password", "1899"}
   };
-  mapstr newData = {{"table","user"}, {"name", "Leo"}};
-  testcon.Add(data);
-  testcon.Edit(newData);
-  vecstr columns = {"table", "name"};
-  mapstr result = testcon.Get(columns);
-  EXPECT_EQ("Leo", result["name"]);
+  testcon.Add(table, data);
+
+  vecstr columns = {"email", "username", "password"};
+
+  string condition = "email = \"mail@mail.ru\"";
+
+  mapstr newdata = {{"username", "Neymar"}};
+
+  testcon.Edit(table, newdata, condition);
+
+  mapstr result = testcon.Get(table, columns, condition)[0];
+
+  data["username"] = "Neymar";
+
+  EXPECT_EQ(data, result);
+
+  testcon.Delete(table, condition);
 }
 
 
@@ -68,128 +94,234 @@ TEST(UserConnector_create, Assert_True) {
 }
 
 
+
+
+
 TEST(UserConnector_AddUser_GetUserInfo, Assert_True) {
-  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
-  mapstr data = {{"table","user"},
+  mapstr data = {
     {"email", "mail@mail.ru"},
-    {"name", "Lionel Messi"},
+    {"username", "Lionel Messi"},
     {"password", "1899"}
   };
-  testcon.AddUser(data);
-  vecstr columns = {"table", "email", "name", "password"};
-  mapstr result = testcon.GetUserInfo(1, columns);
-  EXPECT_EQ(data, result);
-}
-
-
-TEST(UserConnector_EditUser, Assert_True) {
   UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
-  mapstr data = {{"table","user"},
-    {"email", "mail@mail.ru"},
-    {"name", "Lionel Messi"},
-    {"password", "1899"}
-  };
-  testcon.AddUser(data);
-  mapstr newData = {{"table","user"}, {"name", "Leo"}};
-  testcon.EditUser(1, newData);
-  vecstr columns = {"name"};
-  mapstr result = testcon.GetUserInfo(1, columns);
+  string email = "mail@mail.ru";
+	string username = "Lionel Messi";
+	string password = "1899";
+
+	testcon.AddUser(email, username, password);
+  vecstr columns = {"email", "username", "password"};
+  string id = testcon.GetUserId(username);
+  mapstr result = testcon.GetUserInfo(stoi(id), columns);
   EXPECT_EQ(data, result);
-  EXPECT_TRUE(true);
+  
+  testcon.DeleteUser(stoi(id));
 }
 
 
 TEST(UserConnector_DeleteUser, Assert_True) {
+  mapstr data = {};
   UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
-  mapstr data = {{"table","user"},
-    {"email", "mail@mail.ru"},
-    {"name", "Lionel Messi"},
-    {"password", "1899"}
-  };
-  testcon.AddUser(data);
-  testcon.DeleteUser(1);
-  vecstr columns = {"table", "email", "name", "password"};
-  mapstr result = testcon.GetUserInfo(1, columns);
-  mapstr exp = {};
-  EXPECT_EQ(exp, result);
+  string email = "mail@mail.ru";
+	string username = "Lionel Messi";
+	string password = "1899";
+
+	testcon.AddUser(email, username, password);
+  vecstr columns = {"email", "username", "password"};
+  string id = testcon.GetUserId(username);
+  testcon.DeleteUser(stoi(id));
+  mapstr result = {};//testcon.GetUserInfo(stoi(id), columns);
+  EXPECT_EQ(data, result);
+  
   
 }
 
 
-TEST(UserConnector_EstimateFilm_GetEstimation, Assert_True) {
-  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
-  mapstr data = {{"table","user"},
+TEST(UserConnector_EditUser, Assert_True) {
+  mapstr data = {
     {"email", "mail@mail.ru"},
-    {"name", "Lionel Messi"},
+    {"username", "Neymar"},
     {"password", "1899"}
   };
-  testcon.AddUser(data);
-  testcon.EstimateFilm(1, 1, 8);
-  testcon.EstimateFilm(1, 2, 7);
-  mapstr result = testcon.GetUserEstimate(1);
-  mapstr exp = {{"1", "8"}, {"2", "7"}};
-  EXPECT_EQ(exp, result);
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  string email = "mail@mail.ru";
+	string username = "Lionel Messi";
+	string password = "1899";
+
+	testcon.AddUser(email, username, password);
+  vecstr columns = {"email", "username", "password"};
+  string id = testcon.GetUserId(username);
+
+  mapstr newdata = {{"username", "Neymar"}};
+
+  testcon.EditUser(stoi(id), newdata);
+
+  mapstr result = testcon.GetUserInfo(stoi(id), columns);
+  EXPECT_EQ(data, result);
+  
+  testcon.DeleteUser(stoi(id));
+}
+
+
+TEST(UserConnector_EstimateFilm_GetEstimation, Assert_True) {
+
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  string email = "mail@mail.ru";
+	string username = "Lionel Messi";
+	string password = "1899";
+
+	testcon.AddUser(email, username, password);
+  vecstr columns = {"email", "username", "password"};
+  string id = testcon.GetUserId(username);
+  testcon.EstimateFilm(stoi(id), 1, 5);
+  testcon.EstimateFilm(stoi(id), 2, 8);
+  vector<mapstr> est = testcon.GetUserEstimate(stoi(id));
+  vector<mapstr> exp = { { {"estimate", "5"}, {"film_idfilm", "1"} }, { {"estimate", "8"}, {"film_idfilm", "2"} } };
+  //mapstr result = testcon.GetUserInfo(stoi(id), columns);
+  EXPECT_EQ(exp, est);
+  
+  testcon.DeleteUser(stoi(id));
+
+  
 }
 
 
 TEST(UserConnector_CommentFilm, Assert_True) {
   UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
-  mapstr data = {{"table","user"},
-    {"email", "mail@mail.ru"},
-    {"name", "Lionel Messi"},
-    {"password", "1899"}
-  };
-  testcon.AddUser(data);
-  testcon.CommentFilm(1, 1, "comment1");
-  testcon.CommentFilm(1, 1, "comment2");
-  vecstr result = testcon.GetCommentsFilm(1, 1);
-  vecstr exp = {"comment1", "comment2"};
-  EXPECT_EQ(exp, result);
-}
+  string email = "mail@mail.ru";
+	string username = "Lionel Messi";
+	string password = "1899";
+
+	testcon.AddUser(email, username, password);
+  vecstr columns = {"email", "username", "password"};
+  string id = testcon.GetUserId(username);
+  //mapstr result = testcon.GetUserInfo(stoi(id), columns);
+  string txt = "random txt for test lorem ipsum";
+  testcon.CommentFilm(stoi(id), 1, txt);
+  string result = testcon.GetCommentsFilm(stoi(id), 1)[0];
+  EXPECT_EQ(txt, result);
+  
+  testcon.DeleteUser(stoi(id));
 
 
-TEST(UserConnector_DeleteComment, Assert_True) {
-  EXPECT_TRUE(true);
 }
 
 
 TEST(UserConnector_GetUserEstimate, Assert_True) {
-  EXPECT_TRUE(true);
+  vector<mapstr> exp = { {{"film_idfilm", "1"}, {"estimate", "5"}}};
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  string email = "mail@mail.ru";
+	string username = "Lionel Messi";
+	string password = "1899";
+
+	testcon.AddUser(email, username, password);
+  vecstr columns = {"email", "username", "password"};
+  string id = testcon.GetUserId(username);
+  testcon.EstimateFilm(stoi(id), 1, 5);
+
+  vector<mapstr> result = testcon.GetUserEstimate(stoi(id));
+  EXPECT_EQ(exp, result);
+  
+  testcon.DeleteUser(stoi(id));
 }
 
 
 TEST(UserConnector_GetUsersId, Assert_True) {
-  EXPECT_TRUE(true);
+  mapstr data = {
+    {"email", "mail@mail.ru"},
+    {"username", "Lionel Messi"},
+    {"password", "1899"}
+  };
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  string email = "mail@mail.ru";
+	string username = "Lionel Messi";
+	string password = "1899";
+
+	testcon.AddUser(email, username, password);
+  string id = testcon.GetUserId(username);
+  string condition = "username = \"" + username + "\"";
+  string result = testcon.GetUsersId(condition)[0];
+  EXPECT_EQ(id, result);
+  
+  testcon.DeleteUser(stoi(id));
 }
 
 
 TEST(UserConnector_AddUserPreference, Assert_True) {
-  EXPECT_TRUE(true);
+  vector<mapstr> exp { { {"genre", "action"} }, { {"genre", "comedy"} }, 
+                      { {"format", "full"} }, { {"director", "guy richie"} } };
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  string email = "mail@mail.ru";
+	string username = "Lionel Messi";
+	string password = "1899";
+
+	testcon.AddUser(email, username, password);
+  vecstr columns = {"email", "username", "password"};
+  string id = testcon.GetUserId(username);
+  vecstr genre = {"action", "comedy"};
+	vecstr format = {"full"};
+	vecstr director = {"guy richie"};
+  testcon.AddUserPreference(id, "genre", genre);
+  testcon.AddUserPreference(id, "director", director);
+	testcon.AddUserPreference(id, "format", format);
+  vector<mapstr> result = testcon.GetUserPreference(stoi(id));
+  EXPECT_EQ(exp, result);
+  
+  testcon.DeleteUser(stoi(id));
 }
 
 
-TEST(UserConnector_EditUserPreference, Assert_True) {
-  EXPECT_TRUE(true);
-}
 
 
 TEST(UserConnector_DeleteUserPreference, Assert_True) {
-  EXPECT_TRUE(true);
+  vector<mapstr> exp { { {"genre", "action"} }, { {"genre", "comedy"} }, 
+                       { {"director", "guy richie"} } };
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  string email = "mail@mail.ru";
+	string username = "Lionel Messi";
+	string password = "1899";
+
+	testcon.AddUser(email, username, password);
+  vecstr columns = {"email", "username", "password"};
+  string id = testcon.GetUserId(username);
+  vecstr genre = {"action", "comedy"};
+	vecstr format = {"full"};
+	vecstr director = {"guy richie"};
+  testcon.AddUserPreference(id, "genre", genre);
+  testcon.AddUserPreference(id, "director", director);
+	testcon.AddUserPreference(id, "format", format);
+  multimap<string, string> data{};
+  pair<string, string> delfull = make_pair("format", "full");
+  data.insert(delfull);
+  testcon.DeleteUserPreference(stoi(id), data);
+  vector<mapstr> result = testcon.GetUserPreference(stoi(id));
+  EXPECT_EQ(exp, result);
+  
+  testcon.DeleteUser(stoi(id));
 }
 
-
-TEST(UserConnector_GetUserPreference, Assert_True) {
-  EXPECT_TRUE(true);
-}
 
 
 TEST(UserConnector_FindUserEstimation, Assert_True) {
-  EXPECT_TRUE(true);
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  string email = "mail@mail.ru";
+	string username = "Lionel Messi";
+	string password = "1899";
+
+	testcon.AddUser(email, username, password);
+  vecstr columns = {"email", "username", "password"};
+  string id = testcon.GetUserId(username);
+  testcon.EstimateFilm(stoi(id), 1, 5);
+
+  int result = testcon.FindUserEstimation(stoi(id), 1);
+  int exp = 5;
+  EXPECT_EQ(exp, result);
+  
+  testcon.DeleteUser(stoi(id));
 }
 
 
 // FILMCONNECTION
-
+/*
 TEST(FilmConnector_create, Assert_True) {
   FilmConnector x("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
   EXPECT_TRUE(true);
@@ -251,7 +383,7 @@ TEST(FilmConnector_RecountRateFilm, Assert_True) {
 TEST(FilmConnector_FindActor, Assert_True) {
   EXPECT_TRUE(true);
 }
-
+*/
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
