9e862203e7a68c53991fa4c5998c95b892c9a074
Владимир Ловцов <lovets1818@gmail.com>
2021-12-09 17:48:28 +0300

    formatted

--- database_test.cpp
+++ database_test.cpp
@@ -1,19 +1,16 @@
-#include <gtest/gtest.h>
 #include "database.cpp"
 #include <chrono>
+#include <gtest/gtest.h>
 #define private public
-
-
 
 // CONNECTORMYSQL
 TEST(ConnectorMySql_Add_Get, Assert_True) {
-  ConnectorMySql testcon("localhost", "vladimir", "12345678", "Kinopoisdata", 3306);
+  ConnectorMySql testcon("localhost", "vladimir", "12345678", "Kinopoisdata",
+                         3306);
   string table = "user";
-  mapstr data = {
-    {"email", "mail@mail.ru"},
-    {"username", "Lionel Messi"},
-    {"password", "1899"}
-  };
+  mapstr data = {{"email", "mail@mail.ru"},
+                 {"username", "Lionel Messi"},
+                 {"password", "1899"}};
   testcon.Add(table, data);
   vecstr columns = {"email", "username", "password"};
   string condition = "username = \"Lionel Messi\"";
@@ -23,13 +20,12 @@ TEST(ConnectorMySql_Add_Get, Assert_True) {
 }
 
 TEST(ConnectorMySql_Delete, Assert_True) {
-  ConnectorMySql testcon("localhost", "vladimir", "12345678", "Kinopoisdata", 3306);
+  ConnectorMySql testcon("localhost", "vladimir", "12345678", "Kinopoisdata",
+                         3306);
   string table = "user";
-  mapstr data = {
-    {"email", "mail@mail.ru"},
-    {"username", "Lionel Messi"},
-    {"password", "1899"}
-  };
+  mapstr data = {{"email", "mail@mail.ru"},
+                 {"username", "Lionel Messi"},
+                 {"password", "1899"}};
   testcon.Add(table, data);
   vecstr columns = {"email", "username", "password"};
   string condition = "username = \"Lionel Messi\"";
@@ -39,13 +35,12 @@ TEST(ConnectorMySql_Delete, Assert_True) {
 }
 
 TEST(ConnectorMySql_Edit, Assert_True) {
-  ConnectorMySql testcon("localhost", "vladimir", "12345678", "Kinopoisdata", 3306);
+  ConnectorMySql testcon("localhost", "vladimir", "12345678", "Kinopoisdata",
+                         3306);
   string table = "user";
-  mapstr data = {
-    {"email", "mail@mail.ru"},
-    {"username", "Lionel Messi"},
-    {"password", "1899"}
-  };
+  mapstr data = {{"email", "mail@mail.ru"},
+                 {"username", "Lionel Messi"},
+                 {"password", "1899"}};
   testcon.Add(table, data);
 
   vecstr columns = {"email", "username", "password"};
@@ -65,69 +60,59 @@ TEST(ConnectorMySql_Edit, Assert_True) {
   testcon.Delete(table, condition);
 }
 
-
 // USERCONNECTION
 
 TEST(UserConnector_create, Assert_True) {
-  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   EXPECT_TRUE(true);
 }
 
-
-
-
-
 TEST(UserConnector_AddUser_GetUserInfo, Assert_True) {
-  mapstr data = {
-    {"email", "mail@mail.ru"},
-    {"username", "Lionel Messi"},
-    {"password", "1899"}
-  };
-  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  mapstr data = {{"email", "mail@mail.ru"},
+                 {"username", "Lionel Messi"},
+                 {"password", "1899"}};
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string email = "mail@mail.ru";
-	string username = "Lionel Messi";
-	string password = "1899";
+  string username = "Lionel Messi";
+  string password = "1899";
 
-	testcon.AddUser(email, username, password);
+  testcon.AddUser(email, username, password);
   vecstr columns = {"email", "username", "password"};
   string id = testcon.GetUserId(username);
   mapstr result = testcon.GetUserInfo(stoi(id), columns);
   EXPECT_EQ(data, result);
-  
+
   testcon.DeleteUser(stoi(id));
 }
 
-
 TEST(UserConnector_DeleteUser, Assert_True) {
   mapstr data = {};
-  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string email = "mail@mail.ru";
-	string username = "Lionel Messi";
-	string password = "1899";
+  string username = "Lionel Messi";
+  string password = "1899";
 
-	testcon.AddUser(email, username, password);
+  testcon.AddUser(email, username, password);
   vecstr columns = {"email", "username", "password"};
   string id = testcon.GetUserId(username);
   testcon.DeleteUser(stoi(id));
   mapstr result = testcon.GetUserInfo(stoi(id), columns);
   EXPECT_FALSE(result.size());
-  
-  
 }
-
 
 TEST(UserConnector_EditUser, Assert_True) {
   mapstr data = {
-    {"email", "mail@mail.ru"},
-    {"username", "Neymar"},
-    {"password", "1899"}
-  };
-  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+      {"email", "mail@mail.ru"}, {"username", "Neymar"}, {"password", "1899"}};
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string email = "mail@mail.ru";
-	string username = "Lionel Messi";
-	string password = "1899";
+  string username = "Lionel Messi";
+  string password = "1899";
 
-	testcon.AddUser(email, username, password);
+  testcon.AddUser(email, username, password);
   vecstr columns = {"email", "username", "password"};
   string id = testcon.GetUserId(username);
 
@@ -137,157 +122,152 @@ TEST(UserConnector_EditUser, Assert_True) {
 
   mapstr result = testcon.GetUserInfo(stoi(id), columns);
   EXPECT_EQ(data, result);
-  
+
   testcon.DeleteUser(stoi(id));
 }
 
-
 TEST(UserConnector_EstimateFilm_GetEstimation, Assert_True) {
 
-  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string email = "mail@mail.ru";
-	string username = "Lionel Messi";
-	string password = "1899";
+  string username = "Lionel Messi";
+  string password = "1899";
 
-	testcon.AddUser(email, username, password);
+  testcon.AddUser(email, username, password);
   vecstr columns = {"email", "username", "password"};
   string id = testcon.GetUserId(username);
   testcon.EstimateFilm(stoi(id), 1, 5);
   testcon.EstimateFilm(stoi(id), 2, 8);
   vector<mapstr> est = testcon.GetUserEstimate(stoi(id));
-  vector<mapstr> exp = { { {"estimate", "5"}, {"film_idfilm", "1"} }, { {"estimate", "8"}, {"film_idfilm", "2"} } };
-  //mapstr result = testcon.GetUserInfo(stoi(id), columns);
+  vector<mapstr> exp = {{{"estimate", "5"}, {"film_idfilm", "1"}},
+                        {{"estimate", "8"}, {"film_idfilm", "2"}}};
+  // mapstr result = testcon.GetUserInfo(stoi(id), columns);
   EXPECT_EQ(exp, est);
-  
-  testcon.DeleteUser(stoi(id));
 
-  
+  testcon.DeleteUser(stoi(id));
 }
 
-
 TEST(UserConnector_CommentFilm, Assert_True) {
-  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string email = "mail@mail.ru";
-	string username = "Lionel Messi";
-	string password = "1899";
+  string username = "Lionel Messi";
+  string password = "1899";
 
-	testcon.AddUser(email, username, password);
+  testcon.AddUser(email, username, password);
   vecstr columns = {"email", "username", "password"};
   string id = testcon.GetUserId(username);
-  //mapstr result = testcon.GetUserInfo(stoi(id), columns);
+  // mapstr result = testcon.GetUserInfo(stoi(id), columns);
   string txt = "random txt for test lorem ipsum";
   testcon.CommentFilm(stoi(id), 1, txt);
   string result = testcon.GetCommentsFilm(stoi(id), 1)[0];
   EXPECT_EQ(txt, result);
-  
+
   testcon.DeleteUser(stoi(id));
-
-
 }
 
-
 TEST(UserConnector_GetUserEstimate, Assert_True) {
-  vector<mapstr> exp = { {{"film_idfilm", "1"}, {"estimate", "5"}}};
-  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  vector<mapstr> exp = {{{"film_idfilm", "1"}, {"estimate", "5"}}};
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string email = "mail@mail.ru";
-	string username = "Lionel Messi";
-	string password = "1899";
+  string username = "Lionel Messi";
+  string password = "1899";
 
-	testcon.AddUser(email, username, password);
+  testcon.AddUser(email, username, password);
   vecstr columns = {"email", "username", "password"};
   string id = testcon.GetUserId(username);
   testcon.EstimateFilm(stoi(id), 1, 5);
 
   vector<mapstr> result = testcon.GetUserEstimate(stoi(id));
   EXPECT_EQ(exp, result);
-  
+
   testcon.DeleteUser(stoi(id));
 }
 
-
 TEST(UserConnector_GetUsersId, Assert_True) {
-  mapstr data = {
-    {"email", "mail@mail.ru"},
-    {"username", "Lionel Messi"},
-    {"password", "1899"}
-  };
-  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  mapstr data = {{"email", "mail@mail.ru"},
+                 {"username", "Lionel Messi"},
+                 {"password", "1899"}};
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string email = "mail@mail.ru";
-	string username = "Lionel Messi";
-	string password = "1899";
+  string username = "Lionel Messi";
+  string password = "1899";
 
-	testcon.AddUser(email, username, password);
+  testcon.AddUser(email, username, password);
   string id = testcon.GetUserId(username);
   string condition = "username = \"" + username + "\"";
   string result = testcon.GetUsersId(condition)[0];
   EXPECT_EQ(id, result);
-  
+
   testcon.DeleteUser(stoi(id));
 }
-
 
 TEST(UserConnector_AddUserPreference, Assert_True) {
-  vector<mapstr> exp { { {"genre", "action"} }, { {"genre", "comedy"} }, 
-                      { {"format", "full"} }, { {"director", "guy richie"} } };
-  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  vector<mapstr> exp{{{"genre", "action"}},
+                     {{"genre", "comedy"}},
+                     {{"format", "full"}},
+                     {{"director", "guy richie"}}};
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string email = "mail@mail.ru";
-	string username = "Lionel Messi";
-	string password = "1899";
+  string username = "Lionel Messi";
+  string password = "1899";
 
-	testcon.AddUser(email, username, password);
+  testcon.AddUser(email, username, password);
   vecstr columns = {"email", "username", "password"};
   string id = testcon.GetUserId(username);
   vecstr genre = {"action", "comedy"};
-	vecstr format = {"full"};
-	vecstr director = {"guy richie"};
+  vecstr format = {"full"};
+  vecstr director = {"guy richie"};
   testcon.AddUserPreference(id, "genre", genre);
   testcon.AddUserPreference(id, "director", director);
-	testcon.AddUserPreference(id, "format", format);
+  testcon.AddUserPreference(id, "format", format);
   vector<mapstr> result = testcon.GetUserPreference(stoi(id));
   EXPECT_EQ(exp, result);
-  
+
   testcon.DeleteUser(stoi(id));
 }
 
-
-
-
 TEST(UserConnector_DeleteUserPreference, Assert_True) {
-  vector<mapstr> exp { { {"genre", "action"} }, { {"genre", "comedy"} }, 
-                       { {"director", "guy richie"} } };
-  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  vector<mapstr> exp{{{"genre", "action"}},
+                     {{"genre", "comedy"}},
+                     {{"director", "guy richie"}}};
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string email = "mail@mail.ru";
-	string username = "Lionel Messi";
-	string password = "1899";
+  string username = "Lionel Messi";
+  string password = "1899";
 
-	testcon.AddUser(email, username, password);
+  testcon.AddUser(email, username, password);
   vecstr columns = {"email", "username", "password"};
   string id = testcon.GetUserId(username);
   vecstr genre = {"action", "comedy"};
-	vecstr format = {"full"};
-	vecstr director = {"guy richie"};
+  vecstr format = {"full"};
+  vecstr director = {"guy richie"};
   testcon.AddUserPreference(id, "genre", genre);
   testcon.AddUserPreference(id, "director", director);
-	testcon.AddUserPreference(id, "format", format);
+  testcon.AddUserPreference(id, "format", format);
   multimap<string, string> data{};
   pair<string, string> delfull = make_pair("format", "full");
   data.insert(delfull);
   testcon.DeleteUserPreference(stoi(id), data);
   vector<mapstr> result = testcon.GetUserPreference(stoi(id));
   EXPECT_EQ(exp, result);
-  
+
   testcon.DeleteUser(stoi(id));
 }
 
-
-
 TEST(UserConnector_FindUserEstimation, Assert_True) {
-  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  UserConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string email = "mail@mail.ru";
-	string username = "Lionel Messi";
-	string password = "1899";
+  string username = "Lionel Messi";
+  string password = "1899";
 
-	testcon.AddUser(email, username, password);
+  testcon.AddUser(email, username, password);
   vecstr columns = {"email", "username", "password"};
   string id = testcon.GetUserId(username);
   testcon.EstimateFilm(stoi(id), 1, 5);
@@ -295,94 +275,90 @@ TEST(UserConnector_FindUserEstimation, Assert_True) {
   int result = testcon.FindUserEstimation(stoi(id), 1);
   int exp = 5;
   EXPECT_EQ(exp, result);
-  
+
   testcon.DeleteUser(stoi(id));
 }
 
 // FILMCONNECTION
 
 TEST(FilmConnector_AddFilm, Assert_True) {
-  mapstr data = {
-    {"filmname", "Testfilm"},
-    {"year", "2015"},
-    {"country", "USA"},
-    {"director", "gamer"},
-    {"format", "full"}
-  };
-  FilmConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  mapstr data = {{"filmname", "Testfilm"},
+                 {"year", "2015"},
+                 {"country", "USA"},
+                 {"director", "gamer"},
+                 {"format", "full"}};
+  FilmConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string filmname = "Testfilm";
-	int year = 2015;
-	string country = "USA";
-	string format = "full";
-	string director = "gamer";
-	testcon.AddFilm(filmname, year, country, format, director);
-  
+  int year = 2015;
+  string country = "USA";
+  string format = "full";
+  string director = "gamer";
+  testcon.AddFilm(filmname, year, country, format, director);
+
   string condition = " filmname = \"Testfilm\"";
   string id = testcon.GetFilmId(condition)[0];
-  vecstr columns = {"filmname", "year", "director", "country", "format" };
-  
+  vecstr columns = {"filmname", "year", "director", "country", "format"};
+
   mapstr result = testcon.GetFilmInfo(stoi(id), columns);
   EXPECT_EQ(data, result);
   testcon.DeleteFilm(stoi(id));
 }
 
-
 TEST(FilmConnector_DeleteFilm, Assert_True) {
-  FilmConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  FilmConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string filmname = "Testfilm";
-	int year = 2015;
-	string country = "USA";
-	string format = "full";
-	string director = "gamer";
-	testcon.AddFilm(filmname, year, country, format, director);
+  int year = 2015;
+  string country = "USA";
+  string format = "full";
+  string director = "gamer";
+  testcon.AddFilm(filmname, year, country, format, director);
   string condition = " filmname = \"Testfilm\"";
-  vecstr columns = {"filmname", "year", "director", "country", "format" };
+  vecstr columns = {"filmname", "year", "director", "country", "format"};
   string id = testcon.GetFilmId(condition)[0];
   testcon.DeleteFilm(stoi(id));
   mapstr result = testcon.GetFilmInfo(stoi(id), columns);
   EXPECT_FALSE(result.size());
 }
 
-
 TEST(FilmConnector_EditFilm, Assert_True) {
-  mapstr data = {
-    {"filmname", "Testfilm2"},
-    {"year", "2015"},
-    {"country", "USA"},
-    {"director", "gamer"},
-    {"format", "full"}
-  };
-  FilmConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  mapstr data = {{"filmname", "Testfilm2"},
+                 {"year", "2015"},
+                 {"country", "USA"},
+                 {"director", "gamer"},
+                 {"format", "full"}};
+  FilmConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string filmname = "Testfilm";
-	int year = 2015;
-	string country = "USA";
-	string format = "full";
-	string director = "gamer";
-	testcon.AddFilm(filmname, year, country, format, director);
-  
+  int year = 2015;
+  string country = "USA";
+  string format = "full";
+  string director = "gamer";
+  testcon.AddFilm(filmname, year, country, format, director);
+
   string condition = " filmname = \"Testfilm\"";
   string id = testcon.GetFilmId(condition)[0];
-  vecstr columns = {"filmname", "year", "director", "country", "format" };
-
+  vecstr columns = {"filmname", "year", "director", "country", "format"};
 
   mapstr newdata = {{"filmname", "Testfilm2"}};
   testcon.EditFilm(stoi(id), newdata);
-  
+
   mapstr result = testcon.GetFilmInfo(stoi(id), columns);
   EXPECT_EQ(data, result);
   testcon.DeleteFilm(stoi(id));
 }
 
-
 TEST(FilmConnector_AddFilmDetail, Assert_True) {
-  FilmConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  FilmConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string filmname = "Testfilm";
-	int year = 2015;
-	string country = "USA";
-	string format = "full";
-	string director = "gamer";
-	testcon.AddFilm(filmname, year, country, format, director);
-  
+  int year = 2015;
+  string country = "USA";
+  string format = "full";
+  string director = "gamer";
+  testcon.AddFilm(filmname, year, country, format, director);
+
   string condition = " filmname = \"Testfilm\"";
   string id = testcon.GetFilmId(condition)[0];
 
@@ -391,23 +367,24 @@ TEST(FilmConnector_AddFilmDetail, Assert_True) {
   testcon.AddFilmDetail(id, table, genres);
 
   vecstr filmactors = testcon.GetDetail(id, table);
-	for (auto& act : filmactors) {
-		cout << act << endl;
-	}
-  
+  for (auto &act : filmactors) {
+    cout << act << endl;
+  }
+
   EXPECT_EQ(genres, filmactors);
   testcon.DeleteFilm(stoi(id));
 }
 
 TEST(FilmConnector_DeleteFilmDetail, Assert_True) {
-  FilmConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb", 3306);
+  FilmConnector testcon("localhost", "vladimir", "12345678", "Kinopoiskdb",
+                        3306);
   string filmname = "Testfilm";
-	int year = 2015;
-	string country = "USA";
-	string format = "full";
-	string director = "gamer";
-	testcon.AddFilm(filmname, year, country, format, director);
-  
+  int year = 2015;
+  string country = "USA";
+  string format = "full";
+  string director = "gamer";
+  testcon.AddFilm(filmname, year, country, format, director);
+
   string table = "genre";
   vecstr genres = {"art", "action"};
   vecstr expected = {"action"};
@@ -421,6 +398,7 @@ TEST(FilmConnector_DeleteFilmDetail, Assert_True) {
   EXPECT_EQ(expected, filmactors);
   testcon.DeleteFilm(stoi(id));
 }
+
 
 
 int main(int argc, char **argv) {
